11715ade6cb71a831e52312e78c7461be22a4600
BeatLink <git@beatlink.simplelogin.com>
2025-08-25 18:59:57 -0500

    Fix flake

--- flake.nix
+++ flake.nix
@@ -1,152 +1,145 @@
+# Based on https://gist.github.com/guillermofbriceno/abef9f1357329778897ea8dbac17b9db
+
 {
-    description = "Plank Reloaded";
+    description = "Flake for gmusicbrowser with NixOS and Home Manager modules";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs = {
+            url = "github:NixOS/nixpkgs/nixos-unstable";
+        };
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
+    outputs =
+        {
+            self,
+            nixpkgs,
+            home-manager,
+        }:
+        let
+            forAllSystems = nixpkgs.lib.genAttrs [
+                "x86_64-linux"
+                "aarch64-linux"
+            ];
+        in
+        {
+            packages = forAllSystems (
+                system:
+                let
+                    pkgs = import nixpkgs { inherit system; };
 
-    outputs = { self, nixpkgs, ... }: let
-        systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
-        forAllSystems = arch: nixpkgs.lib.genAttrs systems (system: arch system);
-    in {
-        packages = forAllSystems (system:
-            let
-                pkgs = import nixpkgs { inherit system; };
-            in {
-
-
-                ---
-
-# https://gist.github.com/guillermofbriceno/abef9f1357329778897ea8dbac17b9db
-
-{ lib
-, perlPackages
-, fetchFromGitHub
-, gettext
-, multimarkdown
-, wrapGAppsHook
-, gst_all_1
-}:
-
-let
-  inherit (perlPackages) makePerlPath;
-  deps = with perlPackages; [
-    Gtk3 # fix: Can't locate Gtk3.pm in @INC
-    Gtk3ImageView
-    Gtk3SimpleList
-    Cairo # fix: Can't locate Cairo.pm in @INC
-    CairoGObject # fix: Can't locate Cairo/GObject.pm in @INC
-    Glib # fix: Can't locate Glib.pm in @INC
-    GlibObjectIntrospection # fix: 't locate Glib/Object/Introspection.pm in @INC
-    NetDBus
-    XMLTwig
-    XMLParser
-    HTMLParser
-    Pango
-    LocaleGettext
-  ];
-in
-
-perlPackages.buildPerlPackage  {
-  pname = "gmusicbrowser";
-  version = "60d4b6f";
-  src = fetchFromGitHub {
-    owner = "squentin";
-    repo = "gmusicbrowser";
-    rev = "75c410d0dd71f116082aecd3b52af725f670521a";
-    sha256 = "sha256-nZ1/hRrzem5RTeXcGeogvn5PrZoz/U03ZEVPWeYn1Eo=";
-  };
-
-  postInstall = ''
-      find $out -type f -name "*.pod" -delete
-  '';
-  dontConfigure = true; # fix: Can't open perl script "Makefile.PL": No such file or directory
-  doCheck = false; # fix: make: *** No rule to make target 'test'.  Stop.
-  makeFlags = [
-    "prefix=$(out)" # fix: mkdir: cannot create directory '/usr': Permission denied
-  ];
-  outputs = [ "out" ]; # fix: error: builder failed to produce output path for output 'devdoc'
-  # fix: Can't locate Gtk3.pm in @INC
-  preFixup = ''
-    gappsWrapperArgs+=(--prefix PERL5LIB : "${makePerlPath deps}")
-  '';
-  buildInputs = [ 
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gst-libav
-    deps
-  ];
-
-  nativeBuildInputs = [
-    gettext # fix: msgmerge: command not found
-    multimarkdown # fix: markdown: command not found
-    wrapGAppsHook # fix? Typelib file for namespace 'Gtk', version '3.0' not found
-  ];
-
-  meta = with lib; {
-    homepage = "https://github.com/squentin/gmusicbrowser";
-    description = "jukebox for large collections of music";
-    #maintainers = teams.gnome.members;
-    license = licenses.gpl3;
-    platforms = platforms.linux;
-  };
-}
-                ---
-                plank-reloaded = pkgs.stdenv.mkDerivation rec {
-
-                    pname = "plank-reloaded";
-
-                    version = "latest";
-
-                    src = ./.;
-
-                    nativeBuildInputs = [
-                        pkgs.meson
-                        pkgs.ninja
-                        pkgs.pkg-config
-                        pkgs.vala
-                        pkgs.glib
-                        pkgs.bamf
-                        pkgs.wrapGAppsHook  # Added for GSettings support
+                    deps = with pkgs.perlPackages; [
+                        Gtk3
+                        Gtk3ImageView
+                        Gtk3SimpleList
+                        Cairo
+                        CairoGObject
+                        Glib
+                        GlibObjectIntrospection
+                        NetDBus
+                        XMLTwig
+                        XMLParser
+                        HTMLParser
+                        Pango
+                        LocaleGettext
                     ];
+                in
+                {
+                    gmusicbrowser = pkgs.perlPackages.buildPerlPackage rec {
+                        pname = "gmusicbrowser";
+                        version = "unstable-2022-03-02";
 
-                    buildInputs = [
-                        pkgs.gnome-settings-daemon
-                        pkgs.dconf
-                        pkgs.glib
-                        pkgs.git
-                        pkgs.gtk3
-                        pkgs.gnome-menus
-                        pkgs.libgee
-                        pkgs.libwnck
-                        pkgs.pango
-                        pkgs.desktop-file-utils
-                    ];
+                        src = pkgs.fetchFromGitHub {
+                            owner = "squentin";
+                            repo = "gmusicbrowser";
+                            rev = "73089de1a70f537dc790056a50802617ab0a1725";
+                            sha256 = "sha256-i0EZOUxxx1rCa0pKEGzcUDoYNG0al/+bujtkOtWzSAM=";
+                        };
 
-                    # Compile schemas in post-install phase
-                    postInstall = ''
-                        glib-compile-schemas $out/share/glib-2.0/schemas
-                    '';
+                        postInstall = ''
+                            find $out -type f -name "*.pod" -delete
+                        '';
 
-                    patches = [
-                        ./nix-hide-in-pantheon.patch
-                    ];
+                        dontConfigure = true;
+                        doCheck = false;
+                        makeFlags = [ "prefix=$(out)" ];
+                        outputs = [ "out" ];
 
-                    meta = with pkgs.lib; {
-                        description = "A simple dock for X11 environments";
-                        license = licenses.mit;
-                        platforms = platforms.linux;
+                        preFixup = ''
+                            gappsWrapperArgs+=(--prefix PERL5LIB : "${pkgs.perlPackages.makePerlPath deps}")
+                        '';
+
+                        buildInputs = [
+                            pkgs.gst_all_1.gstreamer
+                            pkgs.gst_all_1.gst-plugins-base
+                            pkgs.gst_all_1.gst-plugins-good
+                            pkgs.gst_all_1.gst-plugins-bad
+                            pkgs.gst_all_1.gst-plugins-ugly
+                            pkgs.gst_all_1.gst-libav
+                        ]
+                        ++ deps;
+
+                        nativeBuildInputs = [
+                            pkgs.gettext
+                            pkgs.multimarkdown
+                            pkgs.wrapGAppsHook
+                        ];
+
+                        meta = with pkgs.lib; {
+                            homepage = "https://github.com/squentin/gmusicbrowser";
+                            description = "jukebox for large collections of music";
+                            license = licenses.gpl3;
+                            platforms = platforms.linux;
+                        };
+                    };
+                }
+            );
+
+            # Default package/app
+            defaultPackage = forAllSystems (system: self.packages.${system}.gmusicbrowser);
+
+            apps = forAllSystems (system: {
+                gmusicbrowser = {
+                    type = "app";
+                    program = "${self.packages.${system}.gmusicbrowser}/bin/gmusicbrowser";
+                };
+                default = self.apps.${system}.gmusicbrowser;
+            });
+
+            # NixOS module
+            nixosModules.gmusicbrowser =
+                {
+                    config,
+                    lib,
+                    pkgs,
+                    ...
+                }:
+                with lib;
+                {
+                    options.programs.gmusicbrowser.enable = mkEnableOption "gmusicbrowser music player";
+                    config = mkIf config.programs.gmusicbrowser.enable {
+                        environment.systemPackages = [ self.packages.${pkgs.system}.gmusicbrowser ];
                     };
                 };
-                default = self.packages.${system}.plank-reloaded;
-            });
-        defaultPackage = forAllSystems (system: self.packages.${system}.plank-reloaded);
-        defaultApp = forAllSystems (system: {
-            type = "app";
-            program = "${self.packages.${system}.plank-reloaded}/bin/plank";
-        });
-    };
+            nixosModules.default = self.nixosModules.gmusicbrowser;
+
+            # Home Manager module
+            homeManagerModules.gmusicbrowser =
+                {
+                    config,
+                    lib,
+                    pkgs,
+                    ...
+                }:
+                with lib;
+                {
+                    options.programs.gmusicbrowser.enable = mkEnableOption "gmusicbrowser music player";
+                    config = mkIf config.programs.gmusicbrowser.enable {
+                        home.packages = [ self.packages.${pkgs.system}.gmusicbrowser ];
+                    };
+                };
+            homeManagerModules.default = self.homeManagerModules.gmusicbrowser;
+        };
 }
